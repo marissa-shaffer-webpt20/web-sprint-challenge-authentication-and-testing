dcca5362f27ba7ed2120c81ef74ef7cc649877d5
marissa-shaffer <marissa.shaffer1@gmail.com>
2021-01-24 20:52:52 -0600

    finished tests and checkRole function

--- api/server.js
+++ api/server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const restrict = require('./middleware/restricted.js');
-
-const authRouter = require('./auth/auth-router.js');
-const jokesRouter = require('./jokes/jokes-router.js');
+const authenticate = require('./auth/auth-middleware');
+const authRouter = require('./auth/auth-router');
+const jokesRouter = require('./jokes/jokes-router');
 
 const server = express();
 
@@ -14,6 +13,24 @@ server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
-server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
+server.use('/api/jokes', authenticate, checkRole('user'), jokesRouter);
+
+server.get("/", (req, res) => {
+    res.send("Sprint code for AH");
+});
 
 module.exports = server;
+
+function checkRole(user) {
+    return (req, res, next) => {
+        if (
+            req.decodedToken &&
+            req.decodedToken.role &&
+            req.decodedToken.role.toLowerCase() === user
+        ) {
+            next()
+        } else {
+            res.status(403).json({ message: 'Must be logged in' })
+        }
+    }
+}
